2c3bbc7b8ceda84303af5d240fa38f9d2e60795c
shimsangwoo <sw_shim@hanmail.net>
2023-04-10 23:23:34 +0900

    0000

--- DNG2JPEG.py
+++ DNG2JPEG.py
@@ -8,14 +8,22 @@ path = '.\imageFile\DJI_0583.DNG'
 with rawpy.imread(path) as raw:
     rgb = raw.postprocess()
 
-
-# # 결과값 확인하기
+# # 결과값 확인하기(1번)
 # cv2.imshow('원본파일', rgb)
 # cv2.waitKey()
 # cv2.destroyAllWindows()
 
-# 결과값 저장하기
-cv2.imwrite('.\imageFile\DJI_0583.PNG', rgb,
+## 결과값 확인하기(2번)
+from matplotlib import pyplot as plt
+plt.axis('off')
+plt.imshow(rgb)
+plt.show()
+
+# 이미지 포맷변경 : 이것 안하면 색깔이 뒤바뀌어서 저장된다.
+transImage2 = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+
+# # 결과값 저장하기
+cv2.imwrite('.\imageFile\DJI_0583666.PNG', transImage2,
             [cv2.IMWRITE_PNG_COMPRESSION, 5])
-cv2.imwrite('.\imageFile\DJI_0583.jpg', rgb,
-            [cv2.IMWRITE_JPEG_QUALITY,90])
+# cv2.imwrite('.\imageFile\DJI_0583.jpg', rgb,
+#             [cv2.IMWRITE_JPEG_QUALITY,90])
--- Multi_DNG2JPEG.py
+++ Multi_DNG2JPEG.py
@@ -1,21 +1,25 @@
 import numpy as np
 import cv2
 import rawpy
+# from matplotlib import pyplot as plt
 
 # DNG 파일은 압축될 수 있으므로(무손실 형식일지라도) 먼저 dng 이미지를 디코딩해야 합니다.
 # https://pypi.org/project/rawpy/
 
 ##### 아래의 입력값을 넣어주세요 ####
 startNo = 582 
-endNo = 633
+endNo = 585
 compressure = 90
-##################################
+########################################
 for i in range(startNo, endNo+1):
     img = f'.\imageFile\DJI_0{i}.DNG'
     with rawpy.imread(img) as raw:     # raw데이터를 읽는다.
-        rgb = raw.postprocess()        # 들여쓰기 할 것
+        bgr = raw.postprocess()        # 들여쓰기 할 것
+    rgb = cv2.cvtColor(bgr, cv2.COLOR_BGR2RGB)
     cv2.imwrite(f'.\imageFile\postProcessImage\DJI_0{i}.jpg', rgb, [cv2.IMWRITE_JPEG_QUALITY,compressure])
 # 파일 하나씩 저장(postProcessImage 폴더안)
+
+########################################
 
 # 결과값 저장하기___PNG파일
 # cv2.imwrite(f'.\imageFile\postProcessImage\DJI_0{i}.png', rgb, [cv2.IMWRITE_PNG_COMPRESSION, 5]) # 압축률 0~9
@@ -25,4 +29,5 @@ for i in range(startNo, endNo+1):
 
 # 작성일 : 2023.4.10.
 # 색상이 이상하게 나온다. RGB 컬러 변환으로 생기는 현상으로 생각됨. 푸르게 나옴
-# update 필요 
+# 작성일 : 2023. 4. 11.
+# cv2.cvtcolor()를 이용해서 BGR->RGB로 변환
